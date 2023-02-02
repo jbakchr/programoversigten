99077c46cff27bed995fe72742173a7f228a7f63
Jonas Bak Phillipson <jbakchr@gmail.com>
2023-02-02 22:22:14 +0100

    Added program overview header

---
+++ src/components/ProgramOverview/ProgramOverviewHeader.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const ProgramOverviewHeader = () => {
+  return (
+    <h1 style={{ textAlign: "center", marginBottom: "50px", fontSize: "3vw" }}>
+      Programoversigt
+    </h1>
+  );
+};
