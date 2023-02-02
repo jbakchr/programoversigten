48c0d502a00db4a82853eacad83b41ad70f921ec
Jonas Bak Phillipson <jbakchr@gmail.com>
2023-02-02 21:20:29 +0100

    Added initial mui setup

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,5 +1,6 @@
 import React from "react";
+import { Container } from "@mui/material";
 
 export const HomePage = () => {
-  return <div>Programoversigten</div>;
+  return <Container maxWidth="md">Programoversigten</Container>;
 };
