a42b76217a9fdb3c186811987fe6b204cb70cd2c
Jonas Bak Phillipson <jbakchr@gmail.com>
2023-02-03 08:48:38 +0100

    Added initial program title to program section

--- src/components/ProgramOverviewSection/ProgramOverviewSection.jsx
+++ src/components/ProgramOverviewSection/ProgramOverviewSection.jsx
@@ -1,30 +1,13 @@
 import React from "react";
-import { TextField } from "@mui/material";
-import Grid from "@mui/material/Unstable_Grid2";
 
 import { TimeSlot } from "./TimeSlot";
+import { ProgramTitle } from "./ProgramTitle";
 
 export const ProgramOverviewSection = () => {
   return (
     <>
       <TimeSlot />
-      <Grid xs={9}>
-        <TextField
-          variant="standard"
-          fullWidth
-          placeholder="???"
-          multiline
-          InputProps={{
-            style: {
-              fontFamily: "Covered By Your Grace",
-              fontSize: "35px",
-            },
-          }}
-          inputProps={{
-            style: { textTransform: "uppercase" },
-          }}
-        />
-      </Grid>
+      <ProgramTitle />
     </>
   );
 };
---
+++ src/components/ProgramOverviewSection/ProgramTitle.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { TextField } from "@mui/material";
+import Grid from "@mui/material/Unstable_Grid2";
+
+export const ProgramTitle = () => {
+  return (
+    <Grid xs={9}>
+      <TextField
+        variant="standard"
+        fullWidth
+        placeholder="???"
+        multiline
+        InputProps={{
+          style: {
+            fontFamily: "Covered By Your Grace",
+            fontSize: "35px",
+          },
+        }}
+        inputProps={{
+          style: { textTransform: "uppercase" },
+        }}
+      />
+    </Grid>
+  );
+};
