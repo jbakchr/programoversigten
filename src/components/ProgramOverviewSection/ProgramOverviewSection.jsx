c80dd40e8b534b4910c5f2ad00172c379929ee6d
Jonas Bak Phillipson <jbakchr@gmail.com>
2023-02-03 08:43:57 +0100

    Added initial time slot component

--- src/components/ProgramOverviewSection/ProgramOverviewSection.jsx
+++ src/components/ProgramOverviewSection/ProgramOverviewSection.jsx
@@ -2,36 +2,12 @@ import React from "react";
 import { TextField } from "@mui/material";
 import Grid from "@mui/material/Unstable_Grid2";
 
+import { TimeSlot } from "./TimeSlot";
+
 export const ProgramOverviewSection = () => {
   return (
     <>
-      <Grid
-        xs={3}
-        style={{
-          display: "flex",
-          alignItems: "center",
-          fontSize: "35px",
-          justifyContent: "top",
-        }}
-      >
-        <p style={{ marginRight: "8px" }}>KL.</p>
-        <TextField
-          variant="standard"
-          placeholder="?"
-          InputProps={{
-            style: {
-              fontFamily: "Covered By Your Grace",
-              fontSize: "35px",
-              fontWeight: "bold",
-              width: "70px",
-            },
-          }}
-          inputProps={{
-            style: { textTransform: "uppercase" },
-          }}
-        />
-        <p style={{ marginLeft: "5px", fontWeight: "bold" }}>:</p>
-      </Grid>
+      <TimeSlot />
       <Grid xs={9}>
         <TextField
           variant="standard"
---
+++ src/components/ProgramOverviewSection/TimeSlot.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import Grid from "@mui/material/Unstable_Grid2";
+import { TextField } from "@mui/material";
+
+export const TimeSlot = () => {
+  return (
+    <Grid
+      xs={3}
+      style={{
+        display: "flex",
+        alignItems: "center",
+        fontSize: "35px",
+        justifyContent: "top",
+      }}
+    >
+      <p style={{ marginRight: "8px" }}>KL.</p>
+      <TextField
+        variant="standard"
+        placeholder="?"
+        InputProps={{
+          style: {
+            fontFamily: "Covered By Your Grace",
+            fontSize: "35px",
+            fontWeight: "bold",
+            width: "70px",
+          },
+        }}
+        inputProps={{
+          style: { textTransform: "uppercase" },
+        }}
+      />
+      <p style={{ marginLeft: "5px", fontWeight: "bold" }}>:</p>
+    </Grid>
+  );
+};
